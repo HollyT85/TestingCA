4e0b54403b594dc420d4971625de9147d70a68a2
HollyT85 <>
2023-04-05 15:23:00 +0000

    initial

---
+++ battleship_video.py
@@ -0,0 +1,20 @@
+
+
+HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
+GUESS_BOARD = [[' '] * 8 for x in range(8)]
+
+letters_to_numbers = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7}
+
+
+def board():
+    pass
+
+
+def create_ships():
+    pass
+
+
+def get_ship_location():
+    pass
+
+
